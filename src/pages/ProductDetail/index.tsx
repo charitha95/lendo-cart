109ac650c886f4f5129cc0d76ba7a910022a2709
Charitha Goonewardena <charitha.exe@gmail.com>
2022-06-28 21:36:09 +0530

    fix: .map error in options

--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { PrimaryButton, RadioButtonGroup } from "../../components/UIKit/Button";
-import { Colors } from "../../types";
+import { Colors, Product } from "../../types";
 import ColorCircle from "../../components/UIKit/ColorCircle";
 import classNames from "./style.module.scss";
-import productImg from "../../assets/images/ps4.png";
 import Card from "../../components/UIKit/Card";
 import Rating from "../../components/UIKit/Rating";
 import Divider from "../../components/UIKit/Divider";
 import QuantityInput from "../../components/UIKit/QuantityInput";
 import addToCartIcon from "../../assets/icons/cart-add-light.svg";
 import errorIcon from "../../assets/icons/error.svg";
+import weight from "../../assets/icons/weight.svg";
+import data from "../../data/products.json";
+import formatCurrency from "../../helpers/formatCurrency";
 
 type Variant = {
   name: string;
@@ -17,30 +20,28 @@ type Variant = {
 };
 
 function ProductDetail() {
-  const [color, setColor] = useState("black" as Colors);
+  const [color, setColor] = useState("" as Colors);
   const [quantity, setQuantity] = useState(0);
-  const [variant, setVariant] = useState({} as Variant);
   const [maxQnt, setMaxQnt] = useState(0);
-  const error = false;
+  const [variant, setVariant] = useState({} as Variant);
+  const [product, setProduct] = useState({} as Product);
+
   const canDecrement = quantity !== 0;
   const canIncrement = quantity < maxQnt;
 
-  const product = {
-    id: 1,
-    name: "Philips hue bulb",
-    brand: "Philips",
-    price: "500",
-    available: true,
-    weight: 0.2,
-    options: [
-      { color: "white", power: [6.5, 9.5], quantity: 3 },
-      { color: "red", power: [6.5, 11.5], quantity: 7 }
-    ]
-  };
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      setProduct(data.items[+id - 1]);
+    }
+  }, [id, product]);
 
   const handleColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     // set color
-    const option = product.options.filter((i) => i.color === e.target.value)[0];
+    const option = product.options.filter(
+      (i: any) => i.color === e.target.value
+    )[0];
     setColor(e.target.value as Colors);
 
     // set quantity
@@ -80,73 +81,78 @@ function ProductDetail() {
     <div className={classNames["detail-wrapper"]}>
       <div className="grid">
         <div className={`${classNames["img-section"]} col-12 col-md-6`}>
-          <img src={productImg} alt="product" />
+          <img src={product.imgUrl} alt="product" />
           <ColorCircle size="lg" variant={color} />
         </div>
-        <div className={`${classNames["info-section"]} col-12 col-md-6`}>
-          <Card variant="light" classes="padding-45">
-            <p className={classNames.brand}>Sony</p>
-            <div className={classNames.block}>
-              <h2 className={classNames.name}>Playstation 4</h2>
-              <p className={classNames.price}>1500kr</p>
-            </div>
-            <div className={classNames.review}>
-              <Rating />
-              <p>155 reviews</p>
-            </div>
-            <p className={classNames.desc}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation
-            </p>
-            <Divider />
-            <RadioButtonGroup
-              name="color"
-              options={product.options.map((i) => i.color)}
-              onChange={handleColorChange}
-            />
-            <Divider />
-            {variant && // 👈 null and undefined check
-            Object.keys(variant).length !== 0 ? (
-              <RadioButtonGroup
-                name={variant.name}
-                options={variant.values}
-                onChange={handleOptionChange}
-              />
-            ) : (
-              <p className={classNames["empty-selection"]}>
-                please select a color
-              </p>
-            )}
-
-            <Divider />
-            <p className={classNames.warning}>3 items already in the cart</p>
-            <section className={classNames.action}>
-              <div className={classNames.total}>
-                <QuantityInput
-                  maxQnt={maxQnt}
-                  onDecrement={onDecrement}
-                  onIncrement={onIncrement}
-                  quantity={quantity}
-                />
-                <p>400kr</p>
-              </div>
-              {error ? (
-                <p className={classNames.error}>
-                  <img src={errorIcon} alt="error" /> Sorry! out of stock
+        {product.options ? (
+          <div className={`${classNames["info-section"]} col-12 col-md-6`}>
+            <Card variant="light" classes="padding-45">
+              <p className={classNames.brand}>{product.brand}</p>
+              <div className={classNames.block}>
+                <h2 className={classNames.name}>{product.name}</h2>
+                <p className={classNames.price}>
+                  {formatCurrency(product.price)}
                 </p>
-              ) : (
-                <PrimaryButton
-                  text="Add to cart"
-                  icon={addToCartIcon}
-                  variant="dark"
-                  quantity={quantity.toString()}
-                  onClick={() => {}}
+              </div>
+              <div className={classNames.review}>
+                <Rating />
+                <p>155 reviews</p>
+                <img src={weight} alt="weight" className={classNames.weight} />
+                <p>{product.weight}kg</p>
+              </div>
+              <p className={classNames.desc}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation
+              </p>
+              <Divider />
+              <RadioButtonGroup
+                name="color"
+                options={product.options.map((i: any) => i.color)}
+                onChange={handleColorChange}
+              />
+              <Divider />
+              {variant && Object.keys(variant).length !== 0 ? (
+                <RadioButtonGroup
+                  name={variant.name}
+                  options={variant.values}
+                  onChange={handleOptionChange}
                 />
+              ) : (
+                <p className={classNames["empty-selection"]}>
+                  please select a color
+                </p>
               )}
-            </section>
-          </Card>
-        </div>
+
+              <Divider />
+              <p className={classNames.warning}>3 items already in the cart</p>
+              <section className={classNames.action}>
+                <div className={classNames.total}>
+                  <QuantityInput
+                    maxQnt={maxQnt}
+                    onDecrement={onDecrement}
+                    onIncrement={onIncrement}
+                    quantity={quantity}
+                  />
+                  <p>400kr</p>
+                </div>
+                {!product.available ? (
+                  <p className={classNames.error}>
+                    <img src={errorIcon} alt="error" /> Sorry! out of stock
+                  </p>
+                ) : (
+                  <PrimaryButton
+                    text="Add to cart"
+                    icon={addToCartIcon}
+                    variant="dark"
+                    quantity={quantity.toString()}
+                    onClick={() => {}}
+                  />
+                )}
+              </section>
+            </Card>
+          </div>
+        ) : null}
       </div>
     </div>
   );
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,12 @@
 export type Colors = "black" | "white" | "red" | "orange" | "green";
+
+export type Product = {
+  id: number;
+  name: string;
+  brand: string;
+  price: string;
+  available: boolean;
+  weight: number;
+  imgUrl: string;
+  options: any[];
+};
